8ce4fd2b4682b1502faeed5dd4ec3c7dbe1fde5f
Cao Van Cuong <vancuong.itf@gmail.com>
2019-03-21 14:33:24 +0700

    add logout button

--- resources/views/admin/widget/header.blade.php
+++ resources/views/admin/widget/header.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-primary sticky-top">
     <a class="navbar-brand white-text-hover mr-auto p-2" href="{{ url('admin') }}" style="margin-right: 50px;">Ener Admin Page</a>
-    <form class="form-inline my-2 my-lg-0 p-2" style="float:right;">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0 bg-dark" type="submit">Search</button>
-    </form>
+    <div class="form-inline">
+        <a class="btn btn-primary" href="{{url('admin/logout')}}">Logout</a>
+    </div>
 </nav>
