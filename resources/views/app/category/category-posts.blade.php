ff5f41489ddc9dfac39bff320062bdb0a5c214c2
Cao Van Cuong <vancuong.itf@gmail.com>
2019-04-03 10:34:28 +0700

    update frontend

--- resources/views/app/category/category-posts.blade.php
+++ resources/views/app/category/category-posts.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app') 
 <style type="text/css">
-    
+    .title-lg {
+        display: block;
+    }
+    .title-sm {
+        display: none;
+    }
+    @media (max-width: 767px) {
+        .title-lg {
+            display: none;
+        }
+        .title-sm {
+            display: block;
+        }
+    }
 </style>
 @section('page-title')
 
@@ -14,8 +27,33 @@
 <div class="col-lg-9" style="margin: 0px; padding: 0px;">
     <div class="row row-eq-height" style="box-sizing:border-box; padding: 10px; margin: 0px;">
         <div style="padding-bottom: 5px; border-bottom: 2px solid black; width: 100%; margin-bottom: 20px;">
-            <h3 class="main-text-hover background-orange" style="padding: 5px 10px; margin: 0px;">{{$tag1->name}}</h3>
+        <div class="title-lg">
+            <h3 class="ellipse-1 background-orange" style="padding: 5px 10px; margin: 0px;">
+                <a class="main-text-hover remove-text-decoration" href="{{ url($tag1->route) }}">{{$tag1->name}}</a> 
+                @if ($tag2 != null)
+                    >
+                    <a class="main-text-hover remove-text-decoration" href="{{ url($tag1->route . '/' . $tag2->route) }}">{{$tag2->name}}</a>  
+                    @if ($tag3 != null)
+                        >
+                        <a class="main-text-hover remove-text-decoration" href="{{ url($tag1->route . '/' . $tag2->route . '/' . $tag3->route) }}">{{$tag3->name}}</a>  
+                    @endif
+                @endif
+            </h3>
         </div>
+        <div class="title-sm">
+            <b class="ellipse-1 background-orange" style="padding: 5px 10px; margin: 0px;">
+                <a class="main-text-hover remove-text-decoration" href="{{ url($tag1->route) }}">{{$tag1->name}}</a> 
+                @if ($tag2 != null)
+                    >
+                    <a class="main-text-hover remove-text-decoration" href="{{ url($tag1->route . '/' . $tag2->route) }}">{{$tag2->name}}</a>  
+                    @if ($tag3 != null)
+                        >
+                        <a class="main-text-hover remove-text-decoration" href="{{ url($tag1->route . '/' . $tag2->route . '/' . $tag3->route) }}">{{$tag3->name}}</a>  
+                    @endif
+                @endif
+            </b>
+        </div>  
+    </div>
         @if ($categoryPosts) 
             <div style="width: 100%">
                 @foreach ($categoryPosts->posts as $post)
